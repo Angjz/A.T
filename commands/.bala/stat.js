47da29ae7816508b7feac977d7841fd429344920
AnRyann <ancanthy1@gmail.com>
2021-09-25 17:16:11 +0700

    ub

--- commands/.bala/stat.js
+++ commands/.bala/stat.js
@@ -20,6 +20,7 @@ exports.run4 = (bot, message, args, f1, f2) => {
             { name: 'Tỉ lệ thắng', value: rate + '%', inline: true },
             { name: 'Ba cào', value: bot.info[output].bala.ba_cao + '', inline: true },
             { name: 'Bù', value: bot.info[output].bala.bu + '', inline: true },
+            { name: 'Chuỗi thắng', value: bot.info[output].bala.streak1 + ' ván' },
             { name: 'Tiền', value: bot.info[output].bala.tien + '(VND)' }
         )
     message.channel.send({ embeds:[embed] }).catch(error => {
