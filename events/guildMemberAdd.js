0eb6e710388fb8b6697ea425b1fab3c67f70b391
Ang <ancanthy1@gmail.com>
2021-11-16 13:49:08 +0700

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,7 +4,10 @@ const bot = require("../index.js").bot;
 bot.on('guildMemberAdd', member => {
     if (bot.config[member.guild.id].channel != " "){
         if (member.user.bot){
-            bot.channels.cache.get(bot.config[member.guild.id].channel).send({ content: "<@" + member.user.id + ">, chào mừng bạn đã đến với chốn tuyệt vời này!\n" + "Chúng ta cùng nhau cố gắng nhé! 😄" });
+            bot.channels.cache.get(bot.config[member.guild.id].channel).send({ content: "<@" + member.user.id + ">, chào mừng bạn đã đến với chốn tuyệt vời này!\n" + "Chúng ta cùng nhau cố gắng nhé! 😄" }).catch(error => {
+                if (error.code !== 50013) {
+                    console.error('Lỗi nữaaaaa:', error);
+                }})
             let VT1 = bot.config[member.guild.id].botz;
             for (let i = 0; i < VT1.length; i++){
                 member.roles.add(VT1[i]).catch(error => {
@@ -23,7 +26,10 @@ bot.on('guildMemberAdd', member => {
                 })
             }
         } else{
-            bot.channels.cache.get(bot.config[member.guild.id].channel).send({ content: "<@" + member.user.id + ">, chào mừng bạn đã đến với chốn tuyệt vời này!\n" + "Chúc bạn có những giờ phút vui vẻ nhé! 😄" });
+            bot.channels.cache.get(bot.config[member.guild.id].channel).send({ content: "<@" + member.user.id + ">, chào mừng bạn đã đến với chốn tuyệt vời này!\n" + "Chúc bạn có những giờ phút vui vẻ nhé! 😄" }).catch(error => {
+                if (error.code !== 50013) {
+                    console.error('Lỗi nữaaaaa:', error);
+                }})
             let VT2 = bot.config[member.guild.id].userz;
             for (let j = 0; j < VT2.length; j++){
                 member.roles.add(VT2[j]).catch(error => {
