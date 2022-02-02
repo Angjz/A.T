62f5a2649d33a7ca7892d86660a351b34704cc0a
Ang <ancanthy1@gmail.com>
2022-02-02 10:18:24 +0700

    ~

--- functions/function_bala.js
+++ functions/function_bala.js
@@ -430,7 +430,8 @@ module.exports.ba_la_het = async (bot, message, code) => {
 
 	//chỉnh lại phòng
 	bot.bala_data[code] = {
-		msg: ".",
+		chu: bot.bala_data[code].chu,
+		cuoc: bot.bala_data[code].cuoc,
 		bai: [
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 			11, 12, 13, 14, 15, 16, 17, 18, 19,
