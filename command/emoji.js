b2e5374892e66271e0d2f9956b70689691fea903
Ang <ancanthy1@gmail.com>
2022-06-20 00:41:31 +0700

    Update emoji.js

--- command/emoji.js
+++ command/emoji.js
@@ -5,7 +5,7 @@ exports.run = (bot, message, args, f1, f2) => {
         const embed = new MessageEmbed()
             .setColor('#5432EA') //tím nhạt
             .setTitle('Emoji')
-            .setDescription('Lấy emoji tùy chỉnh của bạn dưới định dạng png và emoji unicode! To hơn! Dễ nhìn hơn! Đẹp hơn!')
+            .setDescription('Lấy emoji tùy chỉnh của bạn dưới định dạng png! To hơn! Dễ nhìn hơn! Đẹp hơn!')
             .addFields(
                 { name: 'Sử dụng', value: bot.config[message.guild.id].prefix + 'emoji <emoji_1> <emoji_2> ... <emoji_n>' },
                 { name: 'Lưu ý', value: 'Kết quả có thể không chính xác nếu dữ liệu đầu vào sai' }
@@ -25,14 +25,6 @@ exports.run = (bot, message, args, f1, f2) => {
                     if (error.code !== 50013) {
                         console.error('Lỗi nữaaaaa:', error);
                     }})
-            } else
-            if (args[count].length == 2){
-                if ( !(args[count].charCodeAt(0) >= 33 && args[count].charCodeAt(0) <= 126) && !(args[count].charCodeAt(1) >= 33 && args[count].charCodeAt(1) <= 126) ){
-                    message.channel.send( args[count] ).catch(error => {
-                    if (error.code !== 50013) {
-                        console.error('Lỗi nữaaaaa:', error);
-                    }})
-                }
             }
             count++;
         }
