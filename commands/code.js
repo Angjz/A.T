fb898e65d74e9615b493f4b5a4940ba087879547
Ang <ancanthy1@gmail.com>
2022-06-19 15:47:31 +0700

    Update code.js

--- commands/code.js
+++ commands/code.js
@@ -4,7 +4,7 @@ exports.run = (bot, message, args, f1, f2) => {
 	const embed = new MessageEmbed()
 		.setColor('#FC8C16') //xanh la nhat
 		.addFields(
-			{ name: 'Mã nguồn của tôi:', value: 'https://github.com/Angjz/A.T' }
+			{ name: 'Mã nguồn của tôi', value: 'https://github.com/Angjz/A.T' }
 		)
 	message.channel.send({ embeds: [embed] }).catch(error => {
 		if (error.code !== 50013) {
