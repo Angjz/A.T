2469fa03754a058d2af57a8972dbf032d417d619
Ang <ancanthy1@gmail.com>
2022-06-20 00:11:49 +0700

    Update emoji.js

--- command/emoji.js
+++ command/emoji.js
@@ -5,7 +5,7 @@ exports.run = (bot, message, args, f1, f2) => {
         const embed = new MessageEmbed()
             .setColor('#5432EA') //tím nhạt
             .setTitle('Emoji')
-            .setDescription('Lấy emoji tùy chỉnh của bạn dưới định dạng png! To hơn! Dễ nhìn hơn! Đẹp hơn!')
+            .setDescription('Lấy emoji __tùy chỉnh__ của bạn dưới định dạng png! To hơn! Dễ nhìn hơn! Đẹp hơn!')
             .addFields(
                 { name: 'Sử dụng', value: bot.config[message.guild.id].prefix + 'emoji <emoji_1> <emoji_2> ... <emoji_n>' },
                 { name: 'Lưu ý', value: 'Kết quả có thể không chính xác nếu dữ liệu đầu vào sai' }
@@ -18,6 +18,10 @@ exports.run = (bot, message, args, f1, f2) => {
     } else{
         let count = 1;
         while (args[count]){
+            if (args[count].length < 5){
+                count++;
+                continue;
+            }
             let emoji = args[count].split(':')[2];
             emoji = emoji.split('>')[0];
             message.channel.send( "https://cdn.discordapp.com/emojis/" + emoji + ".png" ).catch(error => {
