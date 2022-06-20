9b85a39f68535d383d3f21a3705b86a718382ed4
Ang <ancanthy1@gmail.com>
2022-06-20 23:55:50 +0700

    Update emoji.js

--- command/emoji.js
+++ command/emoji.js
@@ -8,7 +8,7 @@ exports.run = (bot, message, args, f1, f2) => {
             .setDescription('Lấy emoji tùy chỉnh của bạn dưới định dạng png! To hơn! Dễ nhìn hơn! Đẹp hơn!')
             .addFields(
                 { name: 'Sử dụng', value: bot.config[message.guild.id].prefix + 'emoji <emoji_1> <emoji_2> ... <emoji_n>' },
-                { name: 'Lưu ý', value: 'Kết quả có thể không chính xác nếu dữ liệu đầu vào sai' }
+                { name: 'Lưu ý', value: 'Kết quả có thể không chính xác nếu dữ liệu đầu vào sai\nBạn nên mở bảng chọn emoji rồi chọn từ đó để cho kết quả chính xác nhất' }
             )
         message.channel.send({ embeds: [embed] }).catch(error => {
             if (error.code !== 50013) {
